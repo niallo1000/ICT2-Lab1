7b1771a08bc68b732d7290a851cde197c054447e
LAPTOP-QP7T4FMV\niall <20082280@mail.wit.ie>
2019-06-21 20:58:29 +0100

    Exercise 2 completed

--- components/exercises/01_staticComponent.js
+++ components/exercises/01_staticComponent.js
@@ -4,7 +4,30 @@ import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 export default class CourseModulesStatic extends Component {
   render() {
     return (
-      <h1> TODO </h1> 
+      <div className="myCSSstyle">
+        <h1>HDip Computer Science - Modules Table</h1>
+          <table className="table table-bordered">
+       <thead>
+          <tr>
+          <th>Name</th>
+          <th>No lectures</th>
+          <th>No practicals</th>
+          </tr>
+      </thead>
+      <tbody >
+        <tr>
+          <td>DevOps</td>
+          <td>2</td>
+          <td>2</td>
+        </tr>
+        <tr>
+          <td>Enterprise Web Dev</td>
+          <td>3</td>
+          <td>2</td>
+        </tr>
+      </tbody >
+</table>
+      </div>
     );
   }
 }
--- components/exercises/02_embeddedVariables.js
+++ components/exercises/02_embeddedVariables.js
@@ -1,8 +1,46 @@
-import React , { Component } from 'react';
-import '../../node_modules/bootstrap/dist/css/bootstrap.css';
+import React, { Component } from "react";
+import "../../node_modules/bootstrap/dist/css/bootstrap.css";
 
-export default class CourseModulesEmbeddedVars extends Component { 
+export default class CourseModulesEmbeddedVars extends Component {
     render() {
-        return <h1> TODO </h1>
+        const course = "HDip Computer Science - Modules table.";
+        const modules = [
+            {
+                name: "DevOps",
+                noLectures: 2,
+                noPracticals: 2
+            },
+            {
+                name: "Enterprise Web Dev",
+                noLectures: 3,
+                noPracticals: 2
+            }
+        ];
+        return (
+            <div>
+                <h1>{course}</h1>
+                <table className="table table-bordered">
+                    <thead>
+                    <tr>
+                        <th>Name</th>
+                        <th>No lectures</th>
+                        <th>No practicals</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    <tr>
+                        <td>{modules[0].name}</td>
+                        <td>{modules[0].noLectures}</td>
+                        <td>{modules[0].noPracticals}</td>
+                    </tr>
+                    <tr>
+                        <td>{modules[1].name}</td>
+                        <td>{modules[1].noLectures}</td>
+                        <td>{modules[1].noPracticals}</td>
+                    </tr>
+                    </tbody>
+                </table>
+            </div>
+        );
     }
 }
